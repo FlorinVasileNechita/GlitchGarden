bca03d1e5371d44c8f264af2450588aa26a21f23
LazyTarget <peter.aslund@hotmail.com>
2016-02-06 23:38:24 +0100

    Scaling, music player

---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Linq;
+
+public class MusicManager : MonoBehaviour {
+
+	public AudioClip[] levelMusicChangeArray;
+
+	private AudioSource _audioSource;
+
+	void Awake () {
+		DontDestroyOnLoad(gameObject);
+	}
+
+	void Start() {
+		_audioSource = GetComponent<AudioSource>();
+	}
+
+	void OnLevelWasLoaded(int level) {
+		var audioClip = levelMusicChangeArray.ElementAtOrDefault(level);
+		if (audioClip != null) {
+			_audioSource.clip = audioClip;
+			_audioSource.Play();
+		}
+	}
+
+}
